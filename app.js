d3526c4ca44790a27cce7096e6cde4cb14d19903
haneen99 <haneenalhaddad99@gmail.com>
2021-06-28 00:49:02 +0300

    country

---
+++ app.js
@@ -0,0 +1,29 @@
+let seattle={minicustomers : 23,
+maximumcustomers :65,
+number :6.3,
+custperhour :function (minimum,maximum){
+    return Math.random() *(maximum,minimum+1)+minimum;)
+}
+totalcookiesperhour :[]
+totalcookies :0,
+render: function(){
+    let body= document.getElementById('body');
+    let ele =textcontent ='seattle';
+    bodyel.child(ulle);
+    for (let i=0;i<hours.lenghth;i++){
+        this.totalcookies.push(math.floor(this.hours(this.minicustomers,
+            this.maximumcustomers)*this.number);
+    }
+    for ( let i = 0; i< totalcookies.length;i++){
+        let el =document .createElement('li');
+        window.li2=el;
+        el.Child(el);
+        this.totalcookies==this.totalcookies +this.totalcookiesperhour[i];
+        console .log(totalcookies);
+
+    }
+
+}
+
+
+
